68f3b056f38c7bbd592c0e7c4f7ebeb7b9fda485
Antti <antti.liakka@gmail.com>
2024-03-20 20:01:20 +0000

    added premake support

---
+++ premake5.lua
@@ -0,0 +1,52 @@
+
+
+project "libsamplerate"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
+    warnings "off"
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+    version = "0.2.2"
+
+    targetdir ("Bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("Bin-Int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "include/samplerate.h",
+
+        "src/common.h",
+        "src/fastest_coeffs.h",
+        "src/high_qual_coeffs.h",
+        "src/mid_qual_coeffs.h",
+
+        "src/samplerate.c",
+        "src/src_linear.c",
+        "src/src_sinc.c",
+        "src/src_zoh.c"
+    }
+
+    includedirs
+    {
+        "include/",
+    }
+
+    defines
+    {
+       "ENABLE_SINC_BEST_CONVERTER",
+       "HAVE_STDBOOL_H",
+       "VERSION=\"" .. version .. "\"",
+       "PACKAGE=\"libsamplerate\""
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+    filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
